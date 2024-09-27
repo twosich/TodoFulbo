a3350f126896425c7f0569a9153118002456842d
John Doe <johndoe@example.com>
2024-09-27 19:19:31 +0200

    add server

--- back/server.js
+++ back/server.js
@@ -1,1 +1,17 @@
-ngc
+const express = require('express');
+
+
+const cors = require('cors');
+
+
+const app = express();
+
+
+// Enable CORS for all routes
+app.use(cors());
+
+
+app.listen(3000, () => {
+   console.log(`Server is up and running on port 3000 ...`);
+});
+
