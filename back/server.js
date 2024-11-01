e004d8fb57ea1075365afbf2d7ab937c7a9075d8
John Doe <johndoe@example.com>
2024-11-01 21:29:49 +0100

    fix server

--- back/server.js
+++ back/server.js
@@ -34,3 +34,44 @@ app.get('/img/:id', (req, res) => {
     // Send the image file
     res.sendFile(imagePath);
   });
+
+  app.post ('/productos', async (req, res) => {
+    try {
+        const productos = await Producto.find ();
+        res.json (productos);
+    } catch (error) {
+        res.status(500).json({ error: error.message});
+    }
+});
+
+app.get('/productos/:nombre', async (req, res) => {
+    try {const producto = await Producto.findByName(req.params.nombre);
+        if (!producto) return res.status(404).json({ error: 'Producto no encontrado' });
+        res.json(producto);
+    } catch (error) {
+        res.status(500).json({ error: error.message });}
+} );
+
+app.put('/productos/:nombre', async (req, res) => {
+    try {
+        const productoActualizado = await Producto.findByNameAndUpdate(req.params.nombre, req.body, { new: true });
+        if (!productoActualizado) return res.status(404).json({ error: 'Producto no encontrado' });
+        res.json(productoActualizado);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+app.delete('/productos/:nombre', async (req, res) => {
+    try {
+        const productoEliminado = await Producto.findByNameAndDelete(req.params.nombre);
+        if (!productoEliminado) return res.status(404).json({ error: 'Producto no encontrado' });
+        res.json({ message: 'Producto eliminado' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
