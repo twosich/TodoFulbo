006acaf8734a62a5a4e9d0f701f659db434f759a
twosich <example@example.com>
2024-11-01 17:35:10 +0100

    added modal

--- src/lib/Items.svelte
+++ src/lib/Items.svelte
@@ -1,32 +1,21 @@
 <script>
     import Img from "./ui/Img.svelte"
+    import Modal from "./ui/Modal.svelte";
+
+    let showModal = $state(false);
 </script>
 
 <div>
     <div class="container mx-auto p-2">   
       <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-4">
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-        <Img />
-
+        <button onclick={() => (showModal = true)}>
+          <Img />
+        </button>
       </div>
+
+    <Modal bind:showModal>
+      
+    </Modal>
+
     </div>
-  </div>
+  </div> 
