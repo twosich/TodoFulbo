ba23b06fa0cc54ec8914b2823e94faddc8d3afb2
John Doe <johndoe@example.com>
2024-11-01 21:21:18 +0100

    added

--- back/db/database.js
+++ back/db/database.js
@@ -18,6 +18,45 @@ const usuario = new moongose.Schema({
     token: {type: String, required: true},
 })
 
-async function readProductos() {
-    const productos = await 
-}
+const Producto = mongoose.model ('Producto', productoSchema);
+
+app.post ('/productos', async (req, res) => {
+    try {
+        const productos = await Producto.find ();
+        res.json (productos);
+    } catch (error) {
+        res.status(500).json({ error: error.message});
+    }
+});
+
+app.get('/productos/:nombre', async (req, res) => {
+    try {const producto = await Producto.findByName(req.params.nombre);
+        if (!producto) return res.status(404).json({ error: 'Producto no encontrado' });
+        res.json(producto);
+    } catch (error) {
+        res.status(500).json({ error: error.message });}
+} );
+
+app.put('/productos/:nombre', async (req, res) => {
+    try {
+        const productoActualizado = await Producto.findByNameAndUpdate(req.params.nombre, req.body, { new: true });
+        if (!productoActualizado) return res.status(404).json({ error: 'Producto no encontrado' });
+        res.json(productoActualizado);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+app.delete('/productos/:nombre', async (req, res) => {
+    try {
+        const productoEliminado = await Producto.findByNameAndDelete(req.params.nombre);
+        if (!productoEliminado) return res.status(404).json({ error: 'Producto no encontrado' });
+        res.json({ message: 'Producto eliminado' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
