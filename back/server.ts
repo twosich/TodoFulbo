9942de341ce6c13dbfa0a158fdf2f63066c0651c
Enzo <example@example.com>
2024-11-08 20:46:21 -0300

    fixed

--- back/server.ts
+++ back/server.ts
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
 
 import Producto from "./db/database"
 
